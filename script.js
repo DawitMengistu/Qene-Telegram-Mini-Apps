b084c9455875f3b3958deb454a5dd57700464675
Dawit Mengistu <davemetadell@gmail.com>
2025-02-08 07:47:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const themeParams = Telegram.WebApp.themeParams;
+
+// Example usage:
+const backgroundColor = themeParams.bg_color;
+const textColor = themeParams.text_color;
+
+const text = document.querySelector(".text")
+
+text.innerHTML = textColor;
